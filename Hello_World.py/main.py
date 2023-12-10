065ff442f0facf87e09d87b54a5f45ab8d37ce11
Sakib722 <sakibdalal73@gmail.com>
2023-12-10 14:01:41 +0530

    for loop with range even number addition

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,16 +1,5 @@
-student_heights = input("Enter height: ").split()  # Using .split function to split the input into list
-print(f"split function: {student_heights}")
-print(f"data type of student_height {type(student_heights)}")
-print(f"length of student_height list: {len(student_heights)}")
-
-length_of_list = len(student_heights)
-
-add = 0
-
-for n in student_heights:
-    add = int(n) + add
-
-avg = round(add/length_of_list)
-print(f"Total of students_height: {add}")
-print(f"Total Students: {length_of_list}")
-print(f"average height of student are: {avg}")
+total = 0
+inp = int(input("Enter input number: ")) + 1
+for n in range(2, inp, 2):
+    total += n
+print(f"total {total}")
