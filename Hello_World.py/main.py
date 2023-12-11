7cb7639781849840fbd8fafac05d7650b485689c
Sakib722 <sakibdalal73@gmail.com>
2023-12-11 10:46:05 +0530

    Fizz Buzz game till 1 to 100

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+print("Fizz Buzz Game from 1 to 100 numbers\n")
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
