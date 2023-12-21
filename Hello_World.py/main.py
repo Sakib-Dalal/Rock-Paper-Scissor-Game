8f6f0ecf18dfffe8e7606093e435c28a54b1d024
IceMupppet <sakibdalal73@gmail.com>
2023-12-21 11:38:48 +0530

    modified Hello_World.py, added Invoice.pdf

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,31 +1,3 @@
-import random
-
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-password = ""
-final_pass = ""
-
-for n in range(0, nr_letters + 1):
-    rand_char = random.randint(0, len(letters)-1)
-    password = password + letters[rand_char]
-
-for n in range(0, nr_symbols + 1):
-    rand_sym = random.randint(0, len(symbols)-1)
-    password = password + symbols[rand_sym]
-
-for n in range(0, nr_numbers + 1):
-    rand_num = random.randint(0, len(numbers)-1)
-    password = password + numbers[rand_num]
-
-for n in password:
-    rand = random.randint(0, len(password)-1)
-    final_pass = final_pass + password[rand]
-
-print(f"Your password is: {final_pass}")
+lst = [1, 2, 3, 3, 9, 6, 45, 45]
+sets = set(lst)
+print(sets)
